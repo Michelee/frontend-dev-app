f5ea45533671e25c77dff2c6b47061d1b5bca27b
Michele Ancheta <michelee.ancheta@gmail.com>
2022-07-03 10:09:41 +0200

    add router, create work page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import Home from './pages/Home';
-import reportWebVitals from './reportWebVitals';
-import './styles/index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import reportWebVitals from "./reportWebVitals";
+import "./styles/index.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="work" element={<Work />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
---
+++ src/pages/Work/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header";
+import styles from "./Work.module.css";
+
+const Work = () => {
+  return (
+    <div className={styles.work}>
+      <Header />
+      njnkn
+    </div>
+  );
+}
+
+export default Work;
